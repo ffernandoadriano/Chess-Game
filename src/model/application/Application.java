2f0a64bf9fb7240170f1a263fa4c905129119a21
Fernando Adriano <lfernando.adriano@gmail.com>
2022-12-28 09:41:54 -0300

    First class: Position

--- src/model/application/Application.java
+++ src/model/application/Application.java
@@ -1,7 +1,10 @@
 package model.application;
 
+import model.boardgame.Position;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
---
+++ src/model/boardgame/Position.java
@@ -0,0 +1,32 @@
+package model.boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position() {
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+    public void setValue(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
