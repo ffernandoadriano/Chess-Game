356dc0b7e6c13094d713840df96d044d2265566d
Fernando Adriano <lfernando.adriano@gmail.com>
2022-12-28 11:28:15 -0300

    Starting to implement Board and Piece

--- src/model/application/Application.java
+++ src/model/application/Application.java
@@ -1,10 +1,9 @@
 package model.application;
 
-import model.boardgame.Position;
+import model.boardgame.Board;
 
 public class Application {
     public static void main(String[] args) {
-        Position position = new Position(3,5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }
